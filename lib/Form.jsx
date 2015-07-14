7bd309b3dfaeec61d8ea5e06530c0cb751612982
Jonas Wu <jonaswu@yahoo-inc.com>
2015-07-14 07:11:46 +0000

    form mock to support component registration if components appear before form

--- lib/Form.jsx
+++ lib/Form.jsx
@@ -1,40 +1,5 @@
 'use strict';
 var React = require('react'),
     _ = require('underscore');
+module.exports = React.createClass(require('./form')());
 
-module.exports = React.createClass({
-    mixins: [
-        require('./Mixin.js')
-    ],
-    isForm: true,
-    register (child) {
-        this.inputs = this.inputs || {};
-        this.childId = this.childId || 0;
-        child.regId = child.props.name || this.childId++;
-        this.inputs[child.regId] = child;
-    },
-    unregister (child) {
-        delete this.inputs[child.regId];
-    },
-    submit (event) {
-        event.stopPropagation();
-        event.preventDefault();
-        var firstErrorComponent = undefined;
-            _.each(
-                this.inputs,
-                    function (component) {
-                        firstErrorComponent = (firstErrorComponent === undefined && component.validate() === false ) ? component: firstErrorComponent;
-                    }
-                );
-        firstErrorComponent === undefined ?
-            this.props.onSubmit.apply(this, arguments) :
-            firstErrorComponent.focus && firstErrorComponent.focus();
-    },
-    render () {
-        return (
-            <form onSubmit={this.submit}>
-                {this.props.children}
-            </form>
-        );
-    }
-});
